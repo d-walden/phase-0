1bc3a45f774e365a5de65f58469d663ed922955d
d-walden <david.ny.walden@gmail.com>
2016-03-06 00:00:45 -1000

    Finish 4.5

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,23 @@
+# Good Guess
+
+# I worked on this challenge by myself.
+
+
+# Your Solution Below
+
+=begin
+
+input: integer
+output: boolean
+
+IF input is equal to 42
+  output true
+ELSE
+  output false
+
+=end
+
+
+def good_guess?(integer)
+  integer == 42
+end
